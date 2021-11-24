8e6244daedeab96fb5eff391a72889869f4fe9fe
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2021-11-24 18:51:23 -0300

    Requisitos de 5 a 6 feitos.

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,35 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
+
   // seu código aqui
+  let classification = {child: 0, adult: 0, senior:0};
+  entrants.forEach((element) => {
+    let { child, adult, senior } = classification;
+    if (element.age < 18) {
+      child += 1;
+      classification.child = child;
+    } else if (element.age >= 18 && element.age < 50) {
+      adult += 1;
+      classification.adult = adult;
+    } else if (element.age >= 50) {
+      senior += 1;
+      classification.senior = senior;
+    }
+  });
+  return classification;
 }
 
 function calculateEntry(entrants) {
+
   // seu código aqui
+  if (entrants === undefined ||
+    entrants.length === undefined || entrants.length === 0) {
+    entrants = [];
+  }
+  const {child, adult, senior} = countEntrants(entrants);
+  const prices = data.prices;
+  return child * prices.child + adult * prices.adult + senior * prices.senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+
+  // seu código
+  let result;
+  if (animal === undefined) {
+    result = {};
+    data.species.forEach((element) => {
+      result[element.name] = data.species.find((element2) => element.name === element2.name).residents.length;
+    });
+  } else if (animal.specie !== undefined && animal.sex === undefined) {
+    result = data.species.find((element) => element.name === animal.specie).residents.length;
+  } else {
+    result = data.species.find((element) => element.name === animal.specie).residents.filter((element) => element.sex === animal.sex).length;
+  }
+  return result
 }
 
 module.exports = countAnimals;
