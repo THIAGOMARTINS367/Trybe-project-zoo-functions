0b8ab4c7cbd888df37a098e1b67f61c62207a70f
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2021-11-24 15:20:03 -0300

    Requisitos de 1 a 2 feitos.

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,9 +1,13 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
 
   // seu código aqui
-  
+  let result = [];
+  ids.forEach((element) => {
+    result.push(data.species.find((element2) => element2.id === element));
+  })
+  return result;
 }
 
 module.exports = getSpeciesByIds;
