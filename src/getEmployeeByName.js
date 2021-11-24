b5f80526324b2772143aaa0e940125a94276aae9
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2021-11-24 16:48:34 -0300

    Requisitos de 3 a 4 feitos.

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
-const data = require('../data/zoo_data');
+const data = require("../data/zoo_data");
 
 function getEmployeeByName(employeeName) {
+  
   // seu código aqui
+  let result = data.employees.find(
+    (element) => element.firstName === employeeName || element.lastName === employeeName
+  );
+  if (result === undefined) {
+    result = {};
+  }
+  return result;
 }
 
 module.exports = getEmployeeByName;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,27 @@
-const data = require('../data/zoo_data');
+const data = require("../data/zoo_data");
 
 function isManager(id) {
   // seu código aqui
+  let array = [];
+  data.employees.forEach((element) => {
+    array.push(...element.managers);
+  });
+
+  return array.some((element) => element === id);
 }
 
 function getRelatedEmployees(managerId) {
+
   // seu código aqui
+  let result;
+  if (isManager(managerId) === true) {
+    result = data.employees
+      .filter((element) => element.managers.includes(managerId))
+      .map((element) => `${element.firstName} ${element.lastName}`);
+  } else {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return result;
 }
 
 module.exports = { isManager, getRelatedEmployees };
