0f97ae8063d10dfaa33bbde59c4818c1f7732a27
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2021-11-26 16:20:25 -0300

    Melhorias feitas para corrigir erros do lint.

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -7,7 +7,19 @@ let animalNames = [];
 let arrayEspecie;
 let speciesByLocation;
 
-const getAnimalMapElse2 = (sorted, specie, sex) => {
+const getAnimalMapElse2_1 = (sorted, specie) => {
+  if (sorted === true) {
+    arrayEspecie[specie.name].sort();
+  }
+  animalNames.push(specie.name);
+  if (result[specie.location] === undefined) {
+    result[specie.location] = [];
+  }
+  result[specie.location].push(arrayEspecie);
+
+}
+
+const getAnimalMapElse2 = (sorted, specie, sex) => {//Refatore esta função para reduzir sua Complexidade Cognitiva de 9 para 5 permitidos.
   if (animalNames.includes(specie.name) === false) {
     arrayEspecie = { [specie.name]: specie.residents.map((element4) => {
       let animalSex;
@@ -16,14 +28,7 @@ const getAnimalMapElse2 = (sorted, specie, sex) => {
       }
       return animalSex;
     }).filter((element3) => element3 !== undefined) };
-    if (sorted === true) {
-      arrayEspecie[specie.name].sort();
-    }
-    animalNames.push(specie.name);
-    if (result[specie.location] === undefined) {
-      result[specie.location] = [];
-    }
-    result[specie.location].push(arrayEspecie);
+    getAnimalMapElse2_1(sorted, specie);
   }
 };
 
@@ -42,8 +47,17 @@ const getAnimalMapElse = (sorted, sex) => {
   return result;
 };
 
-function getAnimalMap(options) {
-  let objectResult = {};
+let objectResult;
+const getAnimalMapTrue = () => {
+  species.forEach((element) => {
+    objectResult[element.location] = data.species
+      .filter((element2) => element2.location === element.location)
+      .map((element3) => element3.location === element.location ? element3.name : '');// função de seta usada ambiguamente com uma expressão condicional.
+  });
+}
+
+function getAnimalMap(options) {// Refatore esta função para reduzir sua Complexidade Cognitiva de 8 para 5 permitidos.
+  objectResult = {};
   let optionsVerified;
   if (options === undefined) {
     optionsVerified = {};
@@ -52,15 +66,16 @@ function getAnimalMap(options) {
   }
   const { includeNames = false, sorted = false, sex = false } = optionsVerified;
   if (includeNames === false) {
-    species.forEach((element) => {
-      objectResult[element.location] = data.species
-        .filter((element2) => element2.location === element.location)
-        .map((element3) => element3.location === element.location ? element3.name : '');// função de seta usada ambiguamente com uma expressão condicional.
-    });
+    getAnimalMapTrue()
+    // species.forEach((element) => {
+    //   objectResult[element.location] = data.species
+    //     .filter((element2) => element2.location === element.location)
+    //     .map((element3) => element3.location === element.location ? element3.name : '');// função de seta usada ambiguamente com uma expressão condicional.
+    // });
   } else {
     objectResult = getAnimalMapElse(sorted, sex);
   }
   return objectResult;
-};
+}
 
 module.exports = getAnimalMap;
