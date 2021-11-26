b984aa450ade485b9a642019024ccbfecc93f906
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2021-11-26 16:04:59 -0300

    Melhorias feitas para corrigir erros do lint.

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -2,39 +2,47 @@ const { species } = require('../data/zoo_data');
 
 const data = require('../data/zoo_data');
 
+let result = {};
+let animalNames = [];
+let arrayEspecie;
+let speciesByLocation;
+
+const getAnimalMapElse2 = (sorted, specie, sex) => {
+  if (animalNames.includes(specie.name) === false) {
+    arrayEspecie = { [specie.name]: specie.residents.map((element4) => {
+      let animalSex;
+      if (sex === element4.sex || sex === false) {
+        animalSex = element4.name;
+      }
+      return animalSex;
+    }).filter((element3) => element3 !== undefined) };
+    if (sorted === true) {
+      arrayEspecie[specie.name].sort();
+    }
+    animalNames.push(specie.name);
+    if (result[specie.location] === undefined) {
+      result[specie.location] = [];
+    }
+    result[specie.location].push(arrayEspecie);
+  }
+};
+
 const getAnimalMapElse = (sorted, sex) => {
-  const result = {};
-  const animalNames = [];
-  let arrayEspecie;
-  let speciesByLocation;
+  result = {};
+  animalNames = [];
+  arrayEspecie = undefined;
+  speciesByLocation = undefined;
   data.species.forEach((element) => {
     speciesByLocation = data.species
       .filter((element2) => element2.location === element.location);
     speciesByLocation.forEach((specie) => {
-      if (animalNames.includes(specie.name) === false) {
-        arrayEspecie = { [specie.name]: specie.residents.map((element4) => {
-          let animalSex;
-          if (sex === element4.sex || sex === false) {
-            animalSex = element4.name;
-          }
-          return animalSex;
-        }).filter((element3) => element3 !== undefined) };
-        if (sorted === true) {
-          arrayEspecie[specie.name].sort();
-        }
-        animalNames.push(specie.name);
-        if (result[specie.location] === undefined) {
-          result[specie.location] = [];
-        }
-        result[specie.location].push(arrayEspecie);
-      }
+      getAnimalMapElse2(sorted, specie, sex);
     });
   });
   return result;
-}
+};
 
 function getAnimalMap(options) {
-  // seu código aqui
   let objectResult = {};
   let optionsVerified;
   if (options === undefined) {
@@ -47,12 +55,12 @@ function getAnimalMap(options) {
     species.forEach((element) => {
       objectResult[element.location] = data.species
         .filter((element2) => element2.location === element.location)
-        .map((element3) => element3.location === element.location ? element3.name : '');
+        .map((element3) => element3.location === element.location ? element3.name : '');// função de seta usada ambiguamente com uma expressão condicional.
     });
   } else {
     objectResult = getAnimalMapElse(sorted, sex);
   }
   return objectResult;
-}
+};
 
 module.exports = getAnimalMap;
